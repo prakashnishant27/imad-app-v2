915e3c8e42e2c69c4797040b2ae058383f81f9dd
prakashnishant27 <prakashnishant27@gmail.com>
2017-02-11 22:28:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,18 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/article1',function(req,res)
+{
+    res.sendFile('Article1 is here!');
+});
+app.get('/article2',function(req,res)
+{
+    res.sendFile('Article2 is here!');
+});
+app.get('/article3',function(req,res)
+{
+    res.sendFile('Article3 is here!');
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
