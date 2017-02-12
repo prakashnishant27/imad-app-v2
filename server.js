e8e0275f39a1ff0c13752629cf9ef8743df54216
prakashnishant27 <prakashnishant27@gmail.com>
2017-02-12 20:06:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ app.get('/article1',function(req,res)
 });
 app.get('/article2',function(req,res)
 {
-    res.send('Article2 is here!');
+  res.sendFile(path.join(__dirname, 'ui', 'article2.html'));
 });
 app.get('/article3',function(req,res)
 {
-    res.send('Article3 is here!');
+  res.sendFile(path.join(__dirname, 'ui', 'article3.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
