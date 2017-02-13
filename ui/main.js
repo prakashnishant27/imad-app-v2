33d42bd14a984603de1d2855a03993622e020c36
prakashnishant27 <prakashnishant27@gmail.com>
2017-02-13 20:11:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,7 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 
 element.innerHTML='Hare Krishna';
+var img=document.getElementById('hari');
+img.onclick=function(){
+img.style.marginLeft='100px';
+};
