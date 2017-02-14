19b69d91eab186fd9b3a56357ae193d67d42ca5e
prakashnishant27 <prakashnishant27@gmail.com>
2017-02-14 20:26:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,3 +6,17 @@ button.onclick=function(){
     var span=document.getElementById('count');
     span.innerHTML=counter.toString();
 };
+
+//submit name
+var nameInput=document.getElementById('name');
+var name =nameInput.value;
+var submit= document.getElementById('submit_btn');
+submit.onclick=function(){
+  var names=['name1','name2','name3','name4'];
+  var list='';
+  for(var i=0;i<names.length;i++){
+      list+= '<li>'+names[i]+'</li>';
+  }
+  var ul=document.getElementByID('namelist');
+  ul.innerHTML=list;
+};
